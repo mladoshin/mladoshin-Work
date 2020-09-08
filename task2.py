301c72c6534e147fd8b95be301340957721a9096
mladoshin <mladoshin@mail.ru>
2020-09-08 18:34:00 +0100

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,17 @@
+import math;
+COVERAGE = 11;
+
+length = float(input("Enter the length of the room: "));
+width = float(input("Enter the width of the room: "));
+height = float(input("Enter the height of the room: "));
+unpArea = float(input("Enter the area of the unpaintable in sq m: "));
+
+
+Area = length*width*2 + length*height*2 + width*height*2 - unpArea;
+volume = math.ceil(Area/COVERAGE);
+print(length);
+print(width);
+print(height);
+print(unpArea);
+print("area: "+str(Area));
+print("paint volume: "+str(volume) + " litres");
