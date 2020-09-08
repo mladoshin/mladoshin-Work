2981f6628f90429ed8d27ca8471067c547f6d265
mladoshin <mladoshin@mail.ru>
2020-09-08 20:33:46 +0100

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,8 @@
+import math
+filledMileage = float(input("Enter the car millieage the last time the car was filled: "));
+currentMileage= float(input("Current Milliage: "));
+tankVolume = float(input("Please enter the tank volume: "));
+
+result = (currentMileage-filledMileage)/(tankVolume*0.22);
+result = round(result, 2)
+print(result);
