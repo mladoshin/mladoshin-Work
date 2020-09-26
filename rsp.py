9532dfac5a3f77f1bb0cc8f64ea5602fa70485a9
mladoshin <mladoshin@mail.ru>
2020-09-26 18:46:36 +0100

    Create rsp.py

---
+++ rsp.py
@@ -0,0 +1,45 @@
+import random
+signs=["R", "S", "P"]
+
+def checkWin(s1, s2):
+    res = True
+    if (s1=="R") and (s2=="P"):
+        res=False
+
+    if(s1=="S") and (s2=="R"):
+        res=False
+
+    if (s1=="P") and (s2=="S"):
+        res=False
+
+    return res
+
+
+def userInput():
+    sign = input("Enter r, s or p: ").upper()
+    try:
+        if (signs.index(sign)):
+            return sign
+    except:
+        print("Error! Enter r, s or p!")
+        return userInput()
+
+    
+
+def main():
+    sign1 = userInput()
+    sign2 = signs[random.randint(0, len(signs)-1)]
+
+    res = checkWin(sign1, sign2)
+    print("You: ",sign1)
+    print("Computer: ",sign2)
+    if (res==True):
+        print("You won!")
+    else: print("You lost!")
+    print("<-------------------->\n")
+    main()
+
+
+main()
+
+
